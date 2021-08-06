73082aee744136ae739a2c7788ba53e51f17baa0
sakshi gupta <sakshigupta82401@gmail.com>
2021-08-06 18:35:44 +0530

    codes documented

--- app/src/main/java/com/rumooursindoyo/moheeeetgupta/ItemsArray.java
+++ app/src/main/java/com/rumooursindoyo/moheeeetgupta/ItemsArray.java
@@ -4,7 +4,13 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
+// Model class for nested JSON objects
+/**
+ * This is used as object of userArray attribute of Results class
+ */
 class ItemsArray {
+
+    // in JSON format, snippet named JSON object is present, so it is exactly implementing that JSON structure.
     @SerializedName("snippet")
     private Snippet snippet;
 
--- app/src/main/java/com/rumooursindoyo/moheeeetgupta/Snippet.java
+++ app/src/main/java/com/rumooursindoyo/moheeeetgupta/Snippet.java
@@ -2,8 +2,13 @@ package com.rumooursindoyo.moheeeetgupta;
 
 import com.google.gson.annotations.SerializedName;
 
+// Model class for nested JSON objects
+/**
+ * This class object is used as attribute of ItemsArray Class
+ */
 class Snippet {
 
+    // topLevelComment , this is inside Snippet JSON object so , to retrive response this structure is implemented.
     @SerializedName("topLevelComment")
     private TopComments comment;
 
