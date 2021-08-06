1cabecc1b48334c7c6a0fb2313ec362a6b58a94f
sakshi gupta <sakshigupta82401@gmail.com>
2021-08-06 17:02:35 +0530

    codes documented

--- app/src/main/java/com/rumooursindoyo/moheeeetgupta/TopComments.java
+++ app/src/main/java/com/rumooursindoyo/moheeeetgupta/TopComments.java
@@ -2,8 +2,13 @@ package com.rumooursindoyo.moheeeetgupta;
 
 import com.google.gson.annotations.SerializedName;
 
+// Model class for nested JSON objects
+/**
+ * This class object is used in Snippet class
+ */
 class TopComments {
 
+    // snippet named JSON object is inside TopComments in JSON format.
     @SerializedName("snippet")
     private Snippetii snippetii;
 
