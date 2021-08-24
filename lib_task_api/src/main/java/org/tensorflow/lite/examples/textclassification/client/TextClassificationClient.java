fdd01b98a940badc40d8256ceef7872bd7ef716f
Shivani <shivanikumari6109@gmail.com>
2021-08-24 16:13:46 +0530

    Code Documented

--- lib_task_api/src/main/java/org/tensorflow/lite/examples/textclassification/client/TextClassificationClient.java
+++ lib_task_api/src/main/java/org/tensorflow/lite/examples/textclassification/client/TextClassificationClient.java
@@ -28,9 +28,14 @@ import org.tensorflow.lite.task.text.nlclassifier.NLClassifier;
 /** Load TfLite model and provide predictions with task api. */
 public class TextClassificationClient {
   private static final String TAG = "TaskApi";
-  private static final String MODEL_PATH = "text_classification.tflite";
+  private static final String MODEL_PATH = "text_classification.tflite"; // path to file to map.
 
   private final Context context;
+
+  /**
+   * NLClassifier API classifies input text into different categories.
+   * it takes a single string as input, performs classification with string and output pairs as classification result.
+   */
 
   NLClassifier classifier;
 
@@ -38,26 +43,52 @@ public class TextClassificationClient {
     this.context = context;
   }
 
+  // tfl model is downloaded first and then
+  // load() is called to bring the modelfile into RAM for each input.
+
   public void load() {
     try {
+
+      // createFromFile creates a memory-mapped file that has specified access mode from a file on disk.
+
       classifier = NLClassifier.createFromFile(context, MODEL_PATH);
     } catch (IOException e) {
       Log.e(TAG, e.getMessage());
     }
   }
 
+  /**
+   * unload() frees RAM and moves modelfile into disk after processing input.
+   * The close() method of Reader Class in Java is used to close the stream and release the resources that were busy in the stream
+   */
+
   public void unload() {
     classifier.close();
     classifier = null;
   }
 
+  /**
+   * 'classify' classifies input text into different categories(here positive and negative)
+   * which is stored in a list.
+   * Category is class of tflite where id, label(display name) and confidence(score) are passed as parameter.
+   */
   public List<Result> classify(String text) {
     List<Category> apiResults = classifier.classify(text);
+
+    // arraylist of size of above list is intialised to iterate and store list elements.
+
     List<Result> results = new ArrayList<>(apiResults.size());
     for (int i = 0; i < apiResults.size(); i++) {
       Category category = apiResults.get(i);
       results.add(new Result("" + i, category.getLabel(), category.getScore()));
     }
+
+    /**
+     * Collections.sort(list) method sorts list element of Comparable type.
+     * After executing above code we'll have the positive and negative probability of input paragraph.
+     * Collections.sort(results) sorts the probability; higher value will be displayed first.
+     */
+
     Collections.sort(results);
     return results;
   }
